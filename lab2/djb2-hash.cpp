a84b885490ff5d966e74de31bec0a2fd5662f3c9
xsun2001 <xcx14@outlook.com>
2021-05-20 13:56:26 +0800

    [lab2] move declaration to header file

--- lab2/djb2-hash.cpp
+++ lab2/djb2-hash.cpp
@@ -1,64 +1,40 @@
-#include "framework/hashtable.h"
+#include "djb2-hash.h"
 
-struct String
+int AsciiString::next()
 {
-	explicit String( char* str ) : data( nullptr ), current_index( 0 )
+	int c = data[current_index];
+	++current_index;
+	return c;
+}
+
+int Utf8String::next()
+{
+	int c1 = data[current_index];
+	if ( ( c1 & 0b10000000 ) == 0 )
 	{
-		int len = strlen( str );
-		data = new char[len];
-		strcpy( data, str );
+		current_index += 1;
+		return c1;
 	}
-	~String() { delete[] data; }
-	virtual int next() = 0;
-
-protected:
-	int current_index;
-	char* data;
-};
-
-struct AsciiString : public String
-{
-	explicit AsciiString( char* str ) : String( str ) {}
-	int next() override
+	else
 	{
-		int c = data[current_index];
-		++current_index;
-		return c;
-	}
-};
-
-struct Utf8String : public String
-{
-	explicit Utf8String( char* str ) : String( str ) {}
-	int next() override
-	{
-		int c1 = data[current_index];
-		if ( ( c1 & 0b10000000 ) == 0 )
+		int c2 = data[current_index + 1];
+		if ( ( c1 >> 5 ) == 0b110 && ( c2 >> 6 ) == 0b10 )
 		{
-			current_index += 1;
-			return c1;
+			current_index += 2;
+			return ( ( c1 & 0b11111 ) << 6 ) | ( c2 & 0b111111 );
 		}
 		else
 		{
-			int c2 = data[current_index + 1];
-			if ( ( c1 >> 5 ) == 0b110 && ( c2 >> 6 ) == 0b10 )
+			int c3 = data[current_index + 1];
+			if ( ( c1 >> 4 ) == 0b1110 && ( c2 >> 6 ) == 0b10 && ( c3 >> 6 ) == 0b10 )
 			{
-				current_index += 2;
-				return ( ( c1 & 0b11111 ) << 6 ) | ( c2 & 0b111111 );
-			}
-			else
-			{
-				int c3 = data[current_index + 1];
-				if ( ( c1 >> 4 ) == 0b1110 && ( c2 >> 6 ) == 0b10 && ( c3 >> 6 ) == 0b10 )
-				{
-					current_index += 3;
-					return ( ( ( ( c1 & 0b1111 ) << 6 ) | ( c2 & 0b111111 ) ) << 6 ) | ( c3 & 0b111111 );
-				}
+				current_index += 3;
+				return ( ( ( ( c1 & 0b1111 ) << 6 ) | ( c2 & 0b111111 ) ) << 6 ) | ( c3 & 0b111111 );
 			}
 		}
-		return 0;
 	}
-};
+	return 0;
+}
 
 unsigned long djb2_hash( String& str )
 {
@@ -71,20 +47,14 @@ unsigned long djb2_hash( String& str )
 	return hash;
 }
 
-struct djb2_ascii_hashing_strategy : public hashing_strategy
+int djb2_ascii_hashing_strategy::operator()( char* str, int N ) override
 {
-	int operator()( char* str, int N ) override
-	{
-		AsciiString string( str );
-		return (int) ( djb2_hash( string ) % (unsigned long) N );
-	}
-};
+	AsciiString string( str );
+	return (int) ( djb2_hash( string ) % (unsigned long) N );
+}
 
-struct djb2_utf8_hashing_strategy : public hashing_strategy
+int djb2_utf8_hashing_strategy::operator()( char* str, int N ) override
 {
-	int operator()( char* str, int N ) override
-	{
-		Utf8String string( str );
-		return (int) ( djb2_hash( string ) % (unsigned long) N );
-	}
-};
+	Utf8String string( str );
+	return (int) ( djb2_hash( string ) % (unsigned long) N );
+}
